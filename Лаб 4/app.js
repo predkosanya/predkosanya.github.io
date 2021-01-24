490b34b7ae9700e8a9b550e73cd4aa3d33e9a950
predkosanya <50735001+predkosanya@users.noreply.github.com>
2021-01-24 16:16:03 +0200

    1

---
+++ Лаб 4/app.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [])
+        .controller('buycontroller', buycontroller)
+        .controller('boughtcontroller', boughtcontroller)
+        .service('itemsservice', itemsservice);
+
+    buycontroller.$inject = ['itemsservice'];
+    function buycontroller(service) {
+        var controller = this;
+        controller.tobuyitems = service.gettobuyitems();
+
+        controller.addtobought = function (itemIndex) { 
+            service.addboughtitem(itemIndex);
+        }
+    };
+
+    boughtcontroller.$inject = ['itemsservice'];
+    function boughtcontroller(service) {
+        var controller = this;
+        controller.boughtItems = service.getboughtitems();
+    };
+
+    function itemsservice() {
+        var service = this;
+
+        var boughtitems = [];
+
+        var tobuyitems = [
+            new item("Яблука", 5),
+            new item("Груші", 6),
+            new item("Сливи", 4),
+            new item("Кавун", 1),
+            new item("Вишні", 3),
+            new item("Банани", 7),
+            new item("Мандарини", 10)
+        ];
+
+        service.addboughtitem = function (shopItemId) {
+            boughtitems.push(tobuyitems[shopItemId]);
+            tobuyitems.splice(shopItemId, 1);
+        };
+
+        service.gettobuyitems = function () {
+            return tobuyitems;
+        };
+
+        service.getboughtitems = function () {
+            return boughtitems;
+        };
+    };
+
+    class item {
+        constructor(name, amount) {
+            this.name = name;
+            this.amount = amount;
+        };
+    };
+
+})();
